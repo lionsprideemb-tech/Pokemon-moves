60adbfac331c9457f915ba00c3ecfce689c330bb
lionsprideemb-tech <lionsprideemb@gmail.com>
2026-09-24 09:50:00 -0500

    Add Gen 6 DS animations 563-577

---
+++ animations/hg-engine/567.s
@@ -0,0 +1,28 @@
+.nds
+.thumb
+
+.include "armips/include/animscriptcmd.s"
+
+.create "build/move/move_anim/0_567", 0
+
+//Sticky Web
+
+a010_567:
+    loadparticlefromspa 0, 543 //electroweb particles from gen 5
+    waitparticle
+
+    addparticle 0, 3, 4
+    addparticle 0, 4, 4
+    playsepan 1919, 117
+    wait 10
+    playsepan 1919, 117
+    wait 5
+    playsepan 1919, 117
+    waitparticle
+
+    unloadparticle 0
+    waitstate
+    end
+    
+
+.close
