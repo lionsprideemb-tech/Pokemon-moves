dc1e5553578b02393192a05732a87178d5b5c513
lionsprideemb-tech <lionsprideemb@gmail.com>
2026-09-24 16:13:47 -0500

    Load final direct animation resolution pass

--- docs/approval/app.js
+++ docs/approval/app.js
@@ -20,7 +20,8 @@ const animationManifests = [
   "manifests/UNTAMED_DS_ANIMATION_PASS_10.csv",
   "manifests/ARMONIA_DS_ANIMATION_PASS_11.csv",
   "manifests/COMMUNITY_DS_ANIMATION_RESCUE_PASS_12.csv",
-  "manifests/COMMUNITY_DS_ANIMATION_PORT_FALLBACK_PASS_13.csv"
+  "manifests/COMMUNITY_DS_ANIMATION_PORT_FALLBACK_PASS_13.csv",
+  "manifests/COMMUNITY_DS_ANIMATION_DIRECT_RESOLUTION_PASS_14.csv"
 ];
 
 const state = {
