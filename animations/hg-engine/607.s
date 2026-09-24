e7fc1812e329f7307e26bd8602e16f1216006440
lionsprideemb-tech <lionsprideemb@gmail.com>
2026-09-24 09:51:07 -0500

    Add Gen 6 DS animations 593-607

---
+++ animations/hg-engine/607.s
@@ -0,0 +1,18 @@
+.nds
+.thumb
+
+.include "armips/include/animscriptcmd.s"
+
+
+.include "armips/include/constants.s"
+
+// electric terrain
+
+.create "build/move/move_anim/0_607", 0
+
+a010_607:
+    changepermanentbg BATTLE_BG_ELECTRIC_TERRAIN, TERRAIN_ELECTRIC_TERRAIN
+    end
+
+
+.close
