bad2db1279cb2d63b555fb20859099e3be4ab8e8
lionsprideemb-tech <lionsprideemb@gmail.com>
2026-09-24 09:44:58 -0500

    Test inline import animations 472-473

---
+++ animations/hg-engine/472.s
@@ -0,0 +1,28 @@
+.nds
+.thumb
+
+.include "armips/include/animscriptcmd.s"
+
+.create "build/move/move_anim/0_472", 0
+
+// wide guard
+
+a010_472:
+    loadparticlefromspa 0, 515
+    waitparticle
+
+    addparticle 0, 2, 3
+
+    addparticle 0, 1, 3
+    waitparticle
+
+    addparticle 0, 0, 3
+    playsepan 1990, -117
+    wait 20
+
+    unloadparticle 0
+    waitstate
+    end
+    
+
+.close
